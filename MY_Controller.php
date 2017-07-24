b6fe647165e23f749d446438819ab4fb5c374593
Erick Daniel Rico Martínez <erick.rico.martinez@gmail.com>
2017-07-24 13:52:50 -0500

    Correciones menores en metodos
    Se corrigió errores en el código.

--- MY_Controller.php
+++ MY_Controller.php
@@ -1,32 +1,43 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
 
+ * @author Erick Daniel Rico Martinez
+ */
 class Functioncode extends CI_Controller
 {
 
-  protected $_session;
+  private $_session= false;
+
+  /**
+   * [private description]
+   * @var [type]
+   */
+  private $_post;
+
+
   function __construct()
   {
     parent::__construct();
-    $this->session->restrictedAcces = _get_session();
+    $this->session->restrictedAcces = $this->_get_session();
   }
-
-  public _set_session($session)
+  /** @$session [boolean] [Valor falso : veradero] */
+  public function _set_session($session)
   {
     $this->_session = $session;
   }
-  public _get_session()
+  public function _get_session()
   {
     return $this->_session;
   }
   public function _get_data_form($fields,$post)
   {
-    $_fields = array();
-    $_post = array();
+    $this->_fields = $fields;
+    $this->_post = $post;
     if (isset($_fields) && isset($_post))
     {
-      if (!empty(_$fields) && !empty($_post))
+      if (!empty($_fields) && !empty($_post))
       {
         foreach ($_fields as $key )
         {
@@ -38,16 +49,19 @@ class Functioncode extends CI_Controller
     }
     return [];
   }
-
+  /**
+   * [login description]
+   * @param  [type] $fields [description]
+   * @return [type]         [description]
+   */
   public function login($fields) {
-    private  $query;
     if (isset($fields))
     {
         if (!empty($fields))
         {
             $_fileds = $this->security->xss_clean($fields);
             $_where = array('userName' => $_fileds['userName'],
-        				'status' =>1);
+                'status' =>1);
             $query = $this->models->select('idUser,userName,password,typeUser','user',$_where);
             if (count($query)>0)
             {
@@ -56,7 +70,7 @@ class Functioncode extends CI_Controller
                   if ($this->encryption->decrypt($consult['password']) === $_fields['password'])
                   {
                     $this->session->idBranchOffice= 1;
-                    this->_set_session(true);
+                    $this->_set_session(true);
                     return true;
                   }
               }
@@ -67,6 +81,16 @@ class Functioncode extends CI_Controller
       return false;
     }
       return false;
+  }
+  public function loadView($settings) {
+      $this->load->view('themplate/head',$settings);
+      if ($this->session->restrictedAcces) {
+        $this->load->view('themplate/menu');
+      }
+      $this->load->view($settings['view'],$settings);
+      if ($this->session->restrictedAcces) {
+        $this->load->view('themplate/script');
+      }
   }
 
 }
